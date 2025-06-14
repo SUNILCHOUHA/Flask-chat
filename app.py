ec0499772b50c2d4668d4b2cff6c9d99a94dcc13
SUNILCHOUHA <chouhansunilm@gmail.com>
2025-06-14 09:17:18 +0530

    correct errors

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] = 'chat-secret-key'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('chat.html')
+    return render_template("chat.html")
 
 @socketio.on('message')
-def handle_message(msg):
-    print(f"Message: {msg}")
+def handleMessage(msg):
+    print("Message:", msg)
     send(msg, broadcast=True)
 
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000)
 
